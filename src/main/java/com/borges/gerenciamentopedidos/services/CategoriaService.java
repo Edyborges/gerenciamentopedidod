7c7b51993d3f99cd50cfcb1f16ae56afbf50fedb
Edyborges <Ed_borg@hotmail.com>
2021-07-12 20:25:42 -0300

    Ajustes no endpoin /categoria/{id}

--- src/main/java/com/borges/gerenciamentopedidos/services/CategoriaService.java
+++ src/main/java/com/borges/gerenciamentopedidos/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.borges.gerenciamentopedidos.domain.Categoria;
 import com.borges.gerenciamentopedidos.repositories.CategoriaRepository;
+import com.borges.gerenciamentopedidos.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,11 @@ public class CategoriaService {
 	
 	public Optional<Categoria> buscar(Integer id) {
 	Optional<Categoria>	 obj = repo.findById(id);
+	
+	if(obj == null) {
+		throw new ObjectNotFoundException("Objeto não encontrtado! Id: " + id
+				+ ", Tipo:" +Categoria.class.getName());
+	}
 		return obj;
 	}
 
